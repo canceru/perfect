a468f6e991a9e3b016f224e2e3ad19ff6e1bf1c3
canceru <1449515027@qq.com>
2021-03-23 23:20:13 +0800

    数组

---
+++ test.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+           for(int i=0;i<array.length;i++){
+               for(int j = 0;j<array[i].length;j++){
+                   if(target == array[i][j]){
+                       return true;
+                   }
+               }
+           }
+        return false;
+    }

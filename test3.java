e59bf627e7cd1365915326fdf7c3e4ff5200da3b
canceru <1449515027@qq.com>
2021-03-26 23:11:13 +0800

    练习

---
+++ test3.java
@@ -0,0 +1,12 @@
+public String replaceSpace (String s) {
+        // write code here
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
